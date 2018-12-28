b7df31bdfe8ede8a85eb1e803b3b578cdacf6938
pierlumanzu <36768270+pierlumanzu@users.noreply.github.com>
2018-12-28 13:57:49 +0100

    Update OutputMilpForPhotos.py

--- OptimizationProblem/OutputMilpForPhotos.py
+++ OptimizationProblem/OutputMilpForPhotos.py
@@ -1,32 +1,6 @@
 from PIL import Image
 import copy
 
-"""def plotSolution(n, bestSolutions, index, informationPhotos, weigths, W, H):
-
-    limsx = (0, W)
-    limsy = (0, H)
-
-    fig1 = plt.figure()
-    ax1 = fig1.add_subplot(111, aspect='equal')
-
-    for j in range(n):
-        x = int(bestSolutions[index][1][3 * j])
-        y = int(bestSolutions[index][1][3 * j + 1])
-        s = bestSolutions[index][1][3 * j + 2]
-
-        ax1.add_patch(patches.Rectangle((x, y),
-                                        int(s * informationPhotos[j][0]),
-                                        (s * informationPhotos[j][1]), fill=None, color='black'))
-        ax1.annotate('{}_{}'.format(j, weigths[j]), (
-            x + int(s * informationPhotos[j][0]) / 2,
-            y + int(s * informationPhotos[j][1]) / 2),
-                     color='black')
-
-    plt.ylim(limsy)
-    plt.xlim(limsx)
-    plt.title("{}° area più grande: {}".format(index + 1, bestSolutions[index][0]))
-    plt.show()"""
-
 def saveImageSolution(n, bestSolutions, index, photos, informationPhotos, imageBackground):
     try:
         imageBackground = copy.copy(imageBackground)
